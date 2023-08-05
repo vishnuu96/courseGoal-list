c2a9c22b89fb21146f5fc283bb026456a32b1d2d
vishnuu96 <riyachowdary37@gmail.com>
2023-08-05 17:05:39 +0530

    added dynamic styling to button and input fields

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -4,7 +4,7 @@ import './Button.css';
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={`button ${props.valid}`} >
       {props.children}
     </button>
   );
